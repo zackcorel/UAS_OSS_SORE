7eba5fef023b81fb9baff15b362be31c9238dded
Zakaria Adiwibowo <38754256+zackcorel@users.noreply.github.com>
2018-06-10 10:32:41 +0700

    ZAKARIA_G231150072_UAS_OSS_SORE.txt

--- UAS_sore.py
+++ UAS_sore.py
@@ -1,18 +1,18 @@
 def Tampil_Tuple(tuple):
-	for i in range(len(tuplee)):
-		print(i+1,'.',tuplee[i])
+    for i in range(len(tuple)): # tuplee diganti dengan tuple
+       print(i+1,'.',tuple[i])   # tuplee diganti dengan tuple  
 
 def Tampil_List(list):
-	for i in listing:
-		print(i)
+    for i in list:              # listing diganti dengan list   
+        print(i)
 
 def Utama():
-	list=[1,2,3,4,5,6,7,8,9]
-	tuple=('Aria Hendrawan','Basworo Ardi Pramono','Khoirudin','April Firman Daru','Whisnumurti')
-	print("\nBerikut adalah angka keberuntungan saya ")
-	Tampil_List(list)
-	print("\n-----------------------------------------")
-	print("Daftar Nama Dosen FTIK USM ")
-	Tampil_Tuple(tuple)
+    list=[1,2,3,4,5,6,7,8,9]
+    tuple=('Aria Hendrawan','Basworo Ardi Pramono','Khoirudin','April Firman Daru','Whisnumurti')
+    print("\nBerikut adalah angka keberuntungan saya ")
+    Tampil_List(list)
+    print("\n-----------------------------------------")
+    print("Daftar Nama Dosen FTIK USM ")
+    Tampil_Tuple(tuple)
 
-Utama(args)
+Utama();         #args dihapus           
